78da4d6bf926fc236de7db6c5236b4a810f9548b
Gino17 <galentro451@gmail.com>
2018-12-04 15:56:17 -0800

    added navbar color changes when user scrolls below jumbotron

--- src/js/script.js
+++ src/js/script.js
@@ -1,38 +1,23 @@
 $(document).ready(function() {
+
     $(document).scroll(function() {
-        let scrollStart = 0;
-        let startChange = $('#main-container');
-        let offset = startChange.offset();
-
-        if (startChange.length) {
-            $(document).scroll(function() {
-                scrollStart = $(this).scrollTop();
-
-                if (scrollStart > offset.top) {
-                    $(".navbar").css({
-                        'background-color': 'white',
-                        'color': 'black'
-                    });
-                } else {
-                    $('.navbar').css('background-color', 'transparent');
-                }
-            })
-        }
-    })
+        $('nav').toggleClass('scrolled', $(this).scrollTop() > 680);
+        $('nav').toggleClass('navbar-light', $(this).scrollTop() > 680);
+    });
 
     // ScrollReval Code
     ScrollReveal().reveal('.headline-1', {
-        delay: 2000,
+        delay: 1000,
         distance: '100px',
         origin: 'left'
     });
 
     ScrollReveal().reveal('.headline-2', {
-        delay: 2000,
+        delay: 1000,
         distance: '100px',
         origin: 'right'
     });
     
-    ScrollReveal().reveal('.title', { delay: 2000 });
-    ScrollReveal().reveal('.tagline', { delay: 2000 });
+    ScrollReveal().reveal('.title', { delay: 1000 });
+    ScrollReveal().reveal('.tagline', { delay: 1000 });
 })
