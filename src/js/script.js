787816d6d1a9dc3c6434d9ba6d28cce15fd5248e
Gino17 <galentro451@gmail.com>
2018-12-03 16:11:28 -0800

    added footer with mock social media buttons and other details

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-//ScrollReval Code
+// ScrollReval Code
 ScrollReveal().reveal('.headline', { delay: 500 });
 ScrollReveal().reveal('.title', { delay: 1000 });
 ScrollReveal().reveal('.tagline', { delay: 1200 });
