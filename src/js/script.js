4b6b8e52ec35b6b62829ef8adf3f06f6034edce1
Gino17 <galentro451@gmail.com>
2018-12-03 19:39:22 -0800

    added a rudimentary navbar color change as user scrolls down, added jquery script link

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,24 @@
-// ScrollReval Code
-ScrollReveal().reveal('.headline', { delay: 500 });
-ScrollReveal().reveal('.title', { delay: 1000 });
-ScrollReveal().reveal('.tagline', { delay: 1200 });
+$(document).ready(function() {
+    $(document).scroll(function() {
+        let scrollStart = 0;
+        let startChange = $('#main-container');
+        let offset = startChange.offset();
+
+        if (startChange.length) {
+            $(document).scroll(function() {
+                scrollStart = $(this).scrollTop();
+
+                if (scrollStart > offset.top) {
+                    $(".navbar").css('background-color', '#f0f0f0');
+                } else {
+                    $('.navbar').css('background-color', 'transparent');
+                }
+            })
+        }
+    })
+
+    // ScrollReval Code
+    ScrollReveal().reveal('.headline', { delay: 500 });
+    ScrollReveal().reveal('.title', { delay: 1000 });
+    ScrollReveal().reveal('.tagline', { delay: 1200 });
+})
