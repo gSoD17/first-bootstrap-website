0fd194b0521dd8c37345ffbe7a1837ba3ed2259a
Gino17 <galentro451@gmail.com>
2018-12-07 15:12:16 -0800

    reworked js code for navbar color change, removed old css code

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,33 @@
 $(document).ready(function() {
 
     $(document).scroll(function() {
-        $('nav').toggleClass('scrolled', $(this).scrollTop() > 680);
-        $('nav').toggleClass('navbar-light', $(this).scrollTop() > 680);
-    });
+        let scrollStart = 0;
+        let startChange = $('#content-container');
+        let offset = startChange.offset();
+
+        if (startChange.length) {
+            $(document).scroll(function() {
+                scrollStart = $(this).scrollTop() + 164;
+
+                if (scrollStart > offset.top) {
+                    $('.navbar').removeClass('navbar-dark')
+                        .removeClass('bg-clear')
+                        .addClass('navbar-light')
+                        .addClass('bg-white');
+                } else if (scrollStart < offset.top) {
+                    $('.navbar').removeClass('navbar-light')
+                        .removeClass('bg-white')
+                        .addClass('navbar-dark')
+                        .addClass('bg-clear')
+                }
+            })
+        }
+    })
+
+    // $(document).scroll(function() {
+    //     $('nav').toggleClass('scrolled', $(this).scrollTop() > 680);
+    //     $('nav').toggleClass('navbar-light', $(this).scrollTop() > 680);
+    // });
 
     // ScrollReval Code
     ScrollReveal().reveal('.headline-1', {
@@ -13,11 +37,9 @@ $(document).ready(function() {
     });
 
     ScrollReveal().reveal('.headline-2', {
-        delay: 1000,
-        distance: '100px',
+        delay: 1200,
+        distance: '0px',
         origin: 'right'
     });
     
-    ScrollReveal().reveal('.title', { delay: 1000 });
-    ScrollReveal().reveal('.tagline', { delay: 1000 });
 })
